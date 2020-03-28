8cc193830a5d4033632926bd8d5b5985840d4803
Matias Pan <matipan@users.noreply.github.com>
2020-03-28 15:16:31 -0300

    Indicaciones para el TP2 (#10)
    * Indicaciones para el TP2
    
    * Correccion
    
    * Se cambia la interfaz y se agrega un ejemplo de cómo
    resolver el programa de manera secuencial
    
    * Correcciones de tp2
    
    * Update README.md
    
    Co-Authored-By: Marcos Nils <marcosnils@gmail.com>
    
    * Update README.md
    
    Co-Authored-By: Marcos Nils <marcosnils@gmail.com>
    
    * Update README.md
    
    Co-Authored-By: Marcos Nils <marcosnils@gmail.com>
    
    * Update README.md
    
    Co-Authored-By: Marcos Nils <marcosnils@gmail.com>
    
    * Update README.md
    
    Co-Authored-By: Marcos Nils <marcosnils@gmail.com>
    
    * Update README.md
    
    Co-Authored-By: Marcos Nils <marcosnils@gmail.com>
    
    Co-authored-by: Marcos Lilljedahl <marcosnils@gmail.com>

---
+++ tp2/tp2.go
@@ -0,0 +1,9 @@
+package tp2
+
+// SumarLista recibe una function sumadora y una lista
+// de numeros. Usando esos parametro retorna la suma de todos
+// los numeros. Si la suma no se puede realizar por algun motivo
+// se retorna un error.
+func SumarLista(sumFunc sumador, numeros ...int) (int, error) {
+	return 0, nil
+}
