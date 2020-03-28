c1fc829e1e67277e851009e3cb263060fae445f6
rjbordon <ricardobordon@gmail.com>
2020-03-28 12:47:12 -0300

    Resolucion del TP #1.

--- tp1/tp1.go
+++ tp1/tp1.go
@@ -1,5 +1,7 @@
 package tp1
 
+import "strconv"
+
 // Producto contiene metodos que nos permiten acceder
 // a atributos que esperamos de un Producto.
 type Producto interface {
@@ -20,21 +22,87 @@ type Carrito struct {
 	Precio int
 }
 
+const (
+	TIENDA_IDX = 0
+	PROD_IDX = 1
+	PRE_IDX = 2
+)
+
 // CalcularPrecios recibe un arreglo de los IDs de productos y calcula,
 // para cada super mercado, cuanto saldria comprar esos productos ahi.
 // Retorna un slice de carritos, donde se tiene uno para cada super mercado.
 func (p Productos) CalcularPrecios(ids ...int) []Carrito {
-	return nil
+	// creo un mapa helper para buscar id sin hacer un range
+	// para cada producto, no se si es mas eficiente
+	// quisiera creer que si lo es en contraste con dos for anidados
+	var mapaIds = make(map[string]int)
+	for _, prodId := range ids {
+		mapaIds[strconv.Itoa(prodId)] = prodId
+	}
+
+	// Inicializo el map con el valor zero de int
+	var mapa = make(map[string]int)
+	for _, entrada := range p {
+		if _, found := mapaIds[entrada[PROD_IDX]]; found == true {
+			pPrecio, _ := strconv.Atoi(entrada[PRE_IDX])
+			// al usar el valor zero de int, no necesito revisar si el elemento
+			// en el mapa existe o no
+			mapa[entrada[TIENDA_IDX]] = mapa[entrada[TIENDA_IDX]] + pPrecio
+		}
+	}
+
+	carritos := []Carrito{}
+	for tienda, precioSumarizado := range mapa {
+		carritos = append(carritos, Carrito{Tienda: tienda, Precio: precioSumarizado})
+	}
+
+	return carritos
 }
 
 // Promedio recibe el id de un producto y retorna el precio promedio
 // de ese producto usando los precios de todos los supermercados.
 func (p Productos) Promedio(idProducto int) float64 {
-	return 0
+	var suma, contador int
+	for _, entrada := range p {
+		if prodId,_ := strconv.Atoi(entrada[PROD_IDX]); prodId == idProducto {
+			precio, _ := strconv.Atoi(entrada[PRE_IDX])
+			suma += precio
+			contador++
+		}
+	}
+
+	var promedio float64
+	if contador > 0 {
+		promedio = float64(suma) / float64(contador)
+	}
+	return promedio
+}
+
+type articulo struct {
+	id, precio int
+}
+
+func (a articulo) ID() int {
+	return a.id
+}
+
+func (a articulo) Precio() int {
+	return a.precio
 }
 
 // BuscarMasBarato recibe un id de producto a buscar y te retorna
 // el producto mas barato que haya encontrado.
 func (p Productos) BuscarMasBarato(idProducto int) (Producto, bool) {
-	return nil, false
+	var art articulo = articulo{id: idProducto}
+	var found bool = false
+	for _, entrada := range p {
+		if prodId,_ := strconv.Atoi(entrada[PROD_IDX]); prodId == idProducto {
+			if precio, _ := strconv.Atoi(entrada[PRE_IDX]); precio < art.precio || !found {
+				art.id = prodId
+				art.precio = precio
+			}
+			found = true
+		}
+	}
+	return art, found
 }
