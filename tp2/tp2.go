69d9bcfc3e0d6d1c90652f681f053a41954f9536
Brunoli <hortelanobruno@gmail.com>
2020-03-28 20:07:02 -0300

    solucion tp2

--- tp2/tp2.go
+++ tp2/tp2.go
@@ -1,9 +1,63 @@
 package tp2
 
+import "fmt"
+
+type pares struct {
+	a, b int
+}
+
+func sumarThread(jobs chan pares, resultadosChan chan int, sumFunc sumador) {
+	for p := range jobs {
+		if p.a != 0 && p.b != 0 {
+			resultadosChan <- sumFunc(p.a, p.b)
+		} else if p.a != 0 {
+			resultadosChan <- p.a
+		} else if p.b != 0 {
+			resultadosChan <- p.b
+		}
+	}
+}
+
+func sumarLista(numeros []int, resultadoChan chan int, sumFunc sumador) {
+	if len(numeros)%2 != 0 {
+		numeros = append(numeros, 0)
+	}
+
+	jobs := make(chan pares, len(numeros))
+	resultadosChan := make(chan int)
+
+	for i := 0; i < len(numeros); i = i + 2 {
+		go sumarThread(jobs, resultadosChan, sumFunc)
+		jobs <- pares{a: numeros[i], b: numeros[i+1]}
+	}
+	close(jobs)
+
+	var resultados []int
+	for i := 0; i < len(numeros)/2; i++ {
+		r := <-resultadosChan
+		resultados = append(resultados, r)
+	}
+	close(resultadosChan)
+
+	if len(resultados) != 2 {
+		sumarLista(resultados, resultadoChan, sumFunc)
+		return
+	}
+	resultadoChan <- sumFunc(resultados[0], resultados[1])
+}
+
 // SumarLista recibe una function sumadora y una lista
 // de numeros. Usando esos parametro retorna la suma de todos
 // los numeros. Si la suma no se puede realizar por algun motivo
 // se retorna un error.
 func SumarLista(sumFunc sumador, numeros ...int) (int, error) {
-	return sumFunc(numeros[0], numeros[1]), nil
+	if len(numeros) == 0 {
+		return 0, fmt.Errorf("No hay numeros para sumar")
+	}
+
+	resultadoChan := make(chan int)
+	go sumarLista(numeros, resultadoChan, sumFunc)
+	resultado := <-resultadoChan
+
+	return resultado, nil
 }
