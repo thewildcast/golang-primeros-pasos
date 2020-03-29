49702a41c9e11f5816d59e0841432379c1103aac
mljcisneros <jonatan.cisneros@mercadolibre.com>
2020-03-29 20:01:45 -0300

    ejercicio 1

--- tp1/tp1.go
+++ tp1/tp1.go
@@ -1,10 +1,21 @@
 package tp1
 
+import (
+		"strconv"
+		 "fmt"
+)
+
 // Producto contiene metodos que nos permiten acceder
 // a atributos que esperamos de un Producto.
 type Producto interface {
 	ID() int
 	Precio() int
+}
+
+type ItemProduct struct {
+	Name string
+	stringId string 
+	precio string
 }
 
 // Productos es una lista de productos donde para cada producto
@@ -20,21 +31,98 @@ type Carrito struct {
 	Precio int
 }
 
+func (ip ItemProduct) ID() int {
+	id, err	 :=	strconv.Atoi(ip.stringId)
+	if err != nil {
+		return 0 
+	}
+	return id
+}
+
+func (ip ItemProduct) Precio() int {
+	precio, err	 :=	strconv.Atoi(ip.precio)
+	if err != nil {
+		return 0
+	}
+	return precio
+}
+
+
 // CalcularPrecios recibe un arreglo de los IDs de productos y calcula,
 // para cada super mercado, cuanto saldria comprar esos productos ahi.
 // Retorna un slice de carritos, donde se tiene uno para cada super mercado.
 func (p Productos) CalcularPrecios(ids ...int) []Carrito {
-	return nil
+	fmt.Println("hola")
+	productos := []ItemProduct{}
+	for _, pitem := range p{
+		productos = append(productos, ItemProduct{pitem[0],pitem[1],pitem[2]})
+	}
+	carritos := map[string]Carrito{}
+	returnCarritos := []Carrito{}
+	for _, itemID := range ids{
+		for _, prod := range productos{
+			if prod.ID() == itemID{
+				c , found := carritos[prod.Name]
+				if found{
+					carritos[prod.Name] = Carrito{prod.Name, c.Precio + prod.Precio()}
+				}else{
+					carritos[prod.Name] = Carrito{prod.Name, prod.Precio()}
+				}
+			     
+			}
+		}
+	}
+	for _, v := range carritos{
+		returnCarritos = append(returnCarritos, v)
+	}
+	
+	return returnCarritos
 }
 
 // Promedio recibe el id de un producto y retorna el precio promedio
 // de ese producto usando los precios de todos los supermercados.
 func (p Productos) Promedio(idProducto int) float64 {
-	return 0
+	productos := []ItemProduct{}
+	for _, pitem := range p{
+		productos = append(productos, ItemProduct{pitem[0],pitem[1],pitem[2]})
+	} 
+	suma, count := 0, 0
+	for _, prod := range productos{
+		if prod.ID() == idProducto{
+			count++
+			suma += prod.Precio()
+		}
+	}
+	if count == 0{
+		return 0.0
+	}
+
+	return float64(suma)/float64(count)
 }
 
 // BuscarMasBarato recibe un id de producto a buscar y te retorna
 // el producto mas barato que haya encontrado.
 func (p Productos) BuscarMasBarato(idProducto int) (Producto, bool) {
-	return nil, false
+	productos := []ItemProduct{}
+	for _, pitem := range p{
+		productos = append(productos, ItemProduct{pitem[0],pitem[1],pitem[2]})
+	}
+	var minorPrice ItemProduct
+	found := false
+	for _, prod := range productos{
+		if prod.ID() == idProducto{
+			if !found{
+				minorPrice = prod
+				found = true
+			}
+			if found && prod.Precio() < minorPrice.Precio(){
+				minorPrice = prod
+			}
+		}
+	}
+	if found{
+		return minorPrice, found
+	}
+	return ItemProduct{stringId: strconv.Itoa(idProducto)}, found
+
 }
