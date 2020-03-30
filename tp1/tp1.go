6c4fde077a2ed9835af33b6695e2a1f8f48a83d2
Gaston Rey <gastonrey@gmail.com>
2020-03-30 16:35:36 +0200

    Initial approach

--- tp1/tp1.go
+++ tp1/tp1.go
@@ -1,40 +1,91 @@
 package tp1
 
-// Producto contiene metodos que nos permiten acceder
-// a atributos que esperamos de un Producto.
+import "strconv"
+
 type Producto interface {
-	ID() int
-	Precio() int
+	BuscarMasBarato()
 }
 
-// Productos es una lista de productos donde para cada producto
-// se sabe el nombre del super mercado, el id y su precio.
-// Esta estructura se puede cargar usando la funcion LeerProductos
-// que carga informacion guardada en `productos.json`.
-type Productos [][]string
+type producto struct {
+	id     int
+	precio float64
+}
 
-// Carrito contiene el nombre de la tienda y el precio final luego
-// de sumar todos los productos.
 type Carrito struct {
-	Tienda string
-	Precio int
+	supermercado string
+	productos    []producto
 }
 
-// CalcularPrecios recibe un arreglo de los IDs de productos y calcula,
-// para cada super mercado, cuanto saldria comprar esos productos ahi.
-// Retorna un slice de carritos, donde se tiene uno para cada super mercado.
-func (p Productos) CalcularPrecios(ids ...int) []Carrito {
-	return nil
+func (c Carrito) CalcularPrecios(productosIds []int) []Carrito {
+	productos, _ := LeerProductos("productos.json")
+	carrito := []Carrito{}
+
+	for _, paramId := range productosIds {
+		for _, rawProducto := range productos {
+			id, _ := strconv.Atoi(rawProducto[1])
+			if paramId == id {
+				precio, _ := strconv.ParseFloat(rawProducto[2], 64)
+				p := producto{id: id, precio: precio}
+
+				c.productos = append(c.productos, p)
+				c.supermercado = rawProducto[0]
+
+				carrito = append(carrito, c)
+			}
+		}
+	}
+
+	return carrito
 }
 
-// Promedio recibe el id de un producto y retorna el precio promedio
-// de ese producto usando los precios de todos los supermercados.
-func (p Productos) Promedio(idProducto int) float64 {
-	return 0
+func (c Carrito) Promedio(productoId int) int {
+	productos, _ := LeerProductos("productos.json")
+	precios := []float64{}
+	var sum float64
+
+	for _, producto := range productos {
+		id, _ := strconv.Atoi(producto[1])
+		if productoId == id {
+			precio, _ := strconv.ParseFloat(producto[2], 64)
+			precios = append(precios, precio)
+		}
+	}
+
+	for i := range precios {
+		sum += precios[i]
+	}
+
+	return int(int(sum) / len(precios))
 }
 
-// BuscarMasBarato recibe un id de producto a buscar y te retorna
-// el producto mas barato que haya encontrado.
-func (p Productos) BuscarMasBarato(idProducto int) (Producto, bool) {
-	return nil, false
+func (c Carrito) BuscarMasBarato(ProductoId int) (Carrito, bool) {
+	productos, _ := LeerProductos("productos.json")
+	carrito := []Carrito{}
+
+	// Armo lista de productos que matchean con el ID
+	for _, pr := range productos {
+		id, _ := strconv.Atoi(pr[1])
+		if ProductoId == id {
+			precio, _ := strconv.ParseFloat(pr[2], 64)
+			p := producto{id: id, precio: precio}
+
+			c.supermercado = pr[0]
+			c.productos = append(c.productos, p)
+
+			carrito = append(carrito, c)
+		}
+	}
+
+	// De un carrito con productos de diferentes supermercados,
+	// Buscamos precio más bajo
+	productoMasBarato := carrito[0] // Tomamos un precio inicial
+	for _, item := range carrito {
+		for _, producto := range item.productos {
+			if producto.precio < productoMasBarato.productos[0].precio {
+				productoMasBarato.supermercado = item.supermercado
+			}
+		}
+	}
+
+	return productoMasBarato, true
 }
