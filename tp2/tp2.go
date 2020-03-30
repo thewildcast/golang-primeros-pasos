cc77d0a629b293e825c4b837d5651f9dab98dbd2
Nicolas Del Piano <ndel314@gmail.com>
2020-03-29 21:10:22 -0300

    Implementar la función `sumFunc`
    Recorre los números dados de a 2, calcula la suma en una goroutine para cada par
    y almacena el resultado en el canal. Luego el bucle consume este
    valor y lo almacena en la variable `suma`.

--- tp2/tp2.go
+++ tp2/tp2.go
@@ -1,9 +1,30 @@
 package tp2
 
+import "fmt"
+
 // SumarLista recibe una function sumadora y una lista
 // de numeros. Usando esos parametro retorna la suma de todos
 // los numeros. Si la suma no se puede realizar por algun motivo
 // se retorna un error.
 func SumarLista(sumFunc sumador, numeros ...int) (int, error) {
-	return sumFunc(numeros[0], numeros[1]), nil
+	suma := 0
+
+	if len(numeros) == 0 {
+		return suma, fmt.Errorf("Error: lista de numeros vacía")
+	}
+
+	c := make(chan int)
+
+	for i := 0; i < len(numeros); i += 2 {
+		go func() {
+			if i+1 >= len(numeros) {
+				c <- numeros[i]
+			} else {
+				c <- sumFunc(numeros[i], numeros[i+1])
+			}
+		}()
+		suma += <-c
+	}
+
+	return suma, nil
 }
