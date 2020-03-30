aefb88e25cac5b4ecbca6e3bc9ddcada03e312df
Leandro Tula <leandro.tula@proveedor.ues21.edu.ar>
2020-03-30 09:43:03 -0300

    first approach to solve 'sum operation' problem

--- tp2/tp2.go
+++ tp2/tp2.go
@@ -1,9 +1,42 @@
 package tp2
 
+import (
+	"errors"
+)
+
 // SumarLista recibe una function sumadora y una lista
 // de numeros. Usando esos parametro retorna la suma de todos
 // los numeros. Si la suma no se puede realizar por algun motivo
 // se retorna un error.
 func SumarLista(sumFunc sumador, numeros ...int) (int, error) {
-	return sumFunc(numeros[0], numeros[1]), nil
+
+	if len(numeros) > 0 {
+
+		input := make(chan int)
+		output := make(chan int)
+
+		go func() {
+
+			for _, value := range numeros {
+				input <- value
+			}
+			close(input)
+		}()
+
+		go func() {
+
+			var sumTotal int = 0
+			for x := range input {
+
+				sumTotal += x
+			}
+			output <- sumTotal
+		}()
+
+		return <-output, nil
+
+	}
+
+	return 0, errors.New("Received empty array to perform sum operation")
+
 }
