db3c27990e52fe3f9698f303edafc0901619d13f
fabian wolfmann <fabian.wolfmann@alumnos.unc.edu.ar>
2020-03-30 16:58:03 -0300

    resolucion tp1

--- tp1/tp1.go
+++ tp1/tp1.go
@@ -1,10 +1,32 @@
 package tp1
 
+import (
+	"strconv"
+)
+
 // Producto contiene metodos que nos permiten acceder
 // a atributos que esperamos de un Producto.
 type Producto interface {
 	ID() int
 	Precio() int
+}
+
+type producto struct {
+	tienda string
+	id     int
+	precio int
+}
+
+func (p producto) Tienda() string {
+	return p.tienda
+}
+
+func (p producto) ID() int {
+	return p.id
+}
+
+func (p producto) Precio() int {
+	return p.precio
 }
 
 // Productos es una lista de productos donde para cada producto
@@ -24,17 +46,62 @@ type Carrito struct {
 // para cada super mercado, cuanto saldria comprar esos productos ahi.
 // Retorna un slice de carritos, donde se tiene uno para cada super mercado.
 func (p Productos) CalcularPrecios(ids ...int) []Carrito {
-	return nil
+	carritos := make(map[string]int)
+	for _, prod := range p {
+		p_id, _ := strconv.Atoi(prod[1])
+		for _, id := range ids {
+			if id == p_id {
+				precio, _ := strconv.Atoi(prod[2])
+				carritos[prod[0]] += precio
+			}
+		}
+	}
+	var carritos_return []Carrito
+
+	for k, v := range carritos {
+		new_carrito := Carrito{k, v}
+		carritos_return = append(carritos_return, new_carrito)
+	}
+	return carritos_return
 }
 
 // Promedio recibe el id de un producto y retorna el precio promedio
 // de ese producto usando los precios de todos los supermercados.
 func (p Productos) Promedio(idProducto int) float64 {
-	return 0
+	var sum_precio int
+	var cant int
+	for _, prod := range p {
+		p_id, _ := strconv.Atoi(prod[1])
+		if idProducto == p_id {
+			precio, _ := strconv.Atoi(prod[2])
+			sum_precio += precio
+			cant++
+		}
+	}
+	var precio_prom float64
+	if cant > 0 {
+		precio_prom = float64(sum_precio) / float64(cant)
+	}
+	return precio_prom
 }
 
 // BuscarMasBarato recibe un id de producto a buscar y te retorna
 // el producto mas barato que haya encontrado.
 func (p Productos) BuscarMasBarato(idProducto int) (Producto, bool) {
-	return nil, false
+	var min_price int
+	var existe bool
+	var tienda string
+	for _, prods := range p {
+		p_id, _ := strconv.Atoi(prods[1])
+		if idProducto == p_id {
+			existe = true
+			precio, _ := strconv.Atoi(prods[2])
+			if precio < min_price || min_price == 0 {
+				min_price = precio
+				tienda = prods[0]
+			}
+		}
+	}
+	pr := producto{tienda, idProducto, min_price}
+	return pr, existe
 }
