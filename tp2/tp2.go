e5b1827f9f3a815d3c2b461cbf0dbeba39cd66d6
Leandro Tula <leandro.tula@proveedor.ues21.edu.ar>
2020-03-31 09:48:51 -0300

    added first version of solution

--- tp2/tp2.go
+++ tp2/tp2.go
@@ -2,6 +2,7 @@ package tp2
 
 import (
 	"errors"
+	"sync"
 )
 
 // SumarLista recibe una function sumadora y una lista
@@ -10,33 +11,47 @@ import (
 // se retorna un error.
 func SumarLista(sumFunc sumador, numeros ...int) (int, error) {
 
-	if len(numeros) > 0 {
+	var wg sync.WaitGroup
+	if len(numeros) == 0 {
 
-		input := make(chan int)
-		output := make(chan int)
-
-		go func() {
-
-			for _, value := range numeros {
-				input <- value
-			}
-			close(input)
-		}()
-
-		go func() {
-
-			var sumTotal int = 0
-			for x := range input {
-
-				sumTotal += x
-			}
-			output <- sumTotal
-		}()
-
-		return <-output, nil
-
+		return 0, errors.New("Received empty array to perform sum operation")
 	}
 
-	return 0, errors.New("Received empty array to perform sum operation")
+	wg.Add(2)
 
+	valuesCh := gen(numeros)
+	wg.Done()
+	output := process(valuesCh, sumFunc)
+	wg.Done()
+
+	wg.Wait()
+
+	return <-output, nil
+
+}
+
+func gen(nums []int) <-chan int {
+
+	out := make(chan int)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+
+}
+
+func process(in <-chan int, sumFunc sumador) <-chan int {
+	out := make(chan int)
+	var t int = 0
+	go func() {
+		for n := range in {
+			t = sumFunc(t, n)
+		}
+		out <- t
+		close(out)
+	}()
+	return out
 }
