e9e93e7ff2c0278aa9d99d61b92d64e0f352b97e
Patricio Lo Duca <loducapatricio@gmail.com>
2020-03-31 23:06:50 -0300

    Resolution TP1 - TP2 & TP3

--- tp3/tp3.go
+++ tp3/tp3.go
@@ -32,6 +32,42 @@ type Resultado struct {
 	Resultado float64
 }
 
+func sumar(op *Operandos, resultado chan<- *Resultado) {
+	resultado <- &Resultado{Operacion: SUMA, Resultado: float64(op.A) + float64(op.B)}
+}
+
+func multiplicar(op *Operandos, resultado chan<- *Resultado) {
+	resultado <- &Resultado{Operacion: MULT, Resultado: float64(op.A) * float64(op.B)}
+}
+
+func dividir(op *Operandos, resultado chan<- *Resultado) {
+	resultado <- &Resultado{Operacion: DIVISION, Resultado: float64(op.A) / float64(op.B)}
+}
+
+func restar(op *Operandos, resultado chan<- *Resultado) {
+	resultado <- &Resultado{Operacion: RESTA, Resultado: float64(op.A) - float64(op.B)}
+}
+
 func Calcular(sumas, mults, divisiones, restas <-chan *Operandos, corte <-chan struct{}) chan *Resultado {
-	return nil
+	resultados := make(chan *Resultado)
+
+	go func() {
+		for {
+			select {
+			case operandos := <-sumas:
+				go sumar(operandos, resultados)
+			case operandos := <-mults:
+				go multiplicar(operandos, resultados)
+			case operandos := <-divisiones:
+				go dividir(operandos, resultados)
+			case operandos := <-restas:
+				go restar(operandos, resultados)
+			case <-corte:
+				close(resultados)
+				return
+			}
+		}
+	}()
+
+	return resultados
 }
