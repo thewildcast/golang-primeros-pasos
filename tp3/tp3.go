d4ba1c70fdabb8ab6e127cd3608712e49576977f
Leandro Tula <leandro.tula@proveedor.ues21.edu.ar>
2020-04-02 22:26:15 -0300

    Added tp3 solution

--- tp3/tp3.go
+++ tp3/tp3.go
@@ -33,5 +33,32 @@ type Resultado struct {
 }
 
 func Calcular(sumas, mults, divisiones, restas <-chan *Operandos, corte <-chan struct{}) chan *Resultado {
-	return nil
+
+	result := make(chan *Resultado)
+
+	go func() {
+
+		for i := 0; i < 4; i++ {
+
+			select {
+
+			case s := <-sumas:
+				result <- &Resultado{Resultado: float64(s.A) + float64(s.B), Operacion: (SUMA)}
+
+			case m := <-mults:
+				result <- &Resultado{Resultado: float64(m.A) * float64(m.B), Operacion: (MULT)}
+
+			case d := <-divisiones:
+				result <- &Resultado{Resultado: float64(d.A) / float64(d.B), Operacion: (DIVISION)}
+
+			case r := <-restas:
+				result <- &Resultado{Resultado: float64(r.A) - float64(r.B), Operacion: (RESTA)}
+
+			}
+		}
+		close(result)
+
+	}()
+
+	return result
 }
