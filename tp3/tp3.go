f9ead48ce43a2060104854618f4e4da28cb5407f
Florencia Alvarez Etcheverry <florencia.a.etcheverry@gmail.com>
2020-04-07 18:07:44 +0200

    Add TP3 implementation

--- tp3/tp3.go
+++ tp3/tp3.go
@@ -33,5 +33,39 @@ type Resultado struct {
 }
 
 func Calcular(sumas, mults, divisiones, restas <-chan *Operandos, corte <-chan struct{}) chan *Resultado {
-	return nil
+	result := make(chan *Resultado)
+	go resolve(sumas, mults, divisiones, restas, corte, result)
+	return result
+}
+
+func resolve(sumas, mults, divisiones, restas <-chan *Operandos, corte <-chan struct{}, result chan *Resultado) {
+	for {
+		select {
+		case sum := <-sumas:
+			result <- &Resultado{
+				Operacion: SUMA,
+				Resultado: float64(sum.A + sum.B),
+			}
+		case mult := <-mults:
+			result <- &Resultado{
+				Operacion: MULT,
+				Resultado: float64(mult.A * mult.B),
+			}
+		case div := <-divisiones:
+			result <- &Resultado{
+				Operacion: DIVISION,
+				Resultado: float64(div.A / div.B),
+			}
+		case dif := <-restas:
+			result <- &Resultado{
+				Operacion: RESTA,
+				Resultado: float64(dif.A - dif.B),
+			}
+		case _, open := <-corte:
+			if !open {
+				close(result)
+				return
+			}
+		}
+	}
 }
