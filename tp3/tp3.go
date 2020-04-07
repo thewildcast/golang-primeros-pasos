b7a4509fd1cc1223d0ed32a6a410f68ad9418ed3
carlos.rios <carlos.rios@efectivosi.com.ar>
2020-04-06 23:19:14 -0300

    Tp3 terminado

--- tp3/tp3.go
+++ tp3/tp3.go
@@ -1,5 +1,10 @@
 package tp3
 
+import ( 
+       // "time"
+        "log"
+        )
+        
 type Op int
 
 const (
@@ -32,6 +37,52 @@ type Resultado struct {
 	Resultado float64
 }
 
+func Procesar(sumas, mults, divisiones, restas <-chan *Operandos, corte <-chan struct{},chanResultado chan *Resultado){
+   // defer close(chanResultado)
+	//timeout := time.After(time.Second)
+
+    for{
+        select{
+            case s:= <- sumas:
+                log.Println("Entro Suma!")
+                resAux := float64(s.A+s.B)
+                chanResultado <- &Resultado {SUMA,resAux}
+            case s:= <- mults:
+                log.Println("Entro Mults!")
+                resAux := float64(s.A*s.B)
+                chanResultado <- &Resultado {MULT,resAux}
+            case s:= <- divisiones:
+                log.Println("Entro Divisiones!")
+                if s.B != 0{
+                  resAux := float64(s.A/s.B)
+                  chanResultado <- &Resultado {DIVISION,resAux}
+                }
+            case s:= <- restas:
+                log.Println("Entro Resta!")
+                resAux := float64(s.A - s.B)
+                chanResultado <- &Resultado {RESTA,resAux}
+            case <- corte:
+                log.Println("CORTEE!")
+                close(chanResultado)
+                //return
+           // case <- timeout:
+            //    log.Println("Timeout!")
+            //    close(chanResultado)
+
+               //default:
+                 //   log.Println("Default!")
+            }
+            log.Println("SALGO DEL SELECT!")
+
+     }
+    close(chanResultado)
+}
+
 func Calcular(sumas, mults, divisiones, restas <-chan *Operandos, corte <-chan struct{}) chan *Resultado {
-	return nil
+    
+	chanResultado := make(chan *Resultado, 4)
+    
+    go Procesar(sumas,mults,divisiones,restas,corte, chanResultado)
+
+    return chanResultado
 }
