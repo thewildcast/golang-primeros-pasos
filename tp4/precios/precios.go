c6421be6bf80f9379acbff9599bca46ae23f9d1a
Pablo Cecconi <pcecconi@gmail.com>
2020-04-08 19:39:04 -0300

    Version con channels!

--- tp4/precios/precios.go
+++ tp4/precios/precios.go
@@ -31,61 +31,116 @@ func init() {
 	cache = make(map[int](map[string]int))
 }
 
-// func preciosWorker(preciosAObtener <-chan precioTienda, precios chan<- precioTienda, errores chan<- error) {
+func preciosWorker(preciosAObtener <-chan precioTienda, precios chan<- precioTienda, errores chan<- error) {
+	for p := range preciosAObtener {
+		fmt.Printf("Getting price of %d from %s\n", p.ID, p.Tienda)
 
-// }
+		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(apiURL, p.Tienda, p.ID), nil)
+		if err != nil {
+			errores <- fmt.Errorf("No se pudo obtener el precio para el producto %d en el supermercado %s", p.ID, p.Tienda)
+			return
+		}
+		res, err := http.DefaultClient.Do(req)
+		if err != nil || res.StatusCode != http.StatusOK {
+			errores <- fmt.Errorf("No se pudo obtener el precio para el producto %d en el supermercado %s", p.ID, p.Tienda)
+		}
+		var precio precioTienda
+		json.NewDecoder(res.Body).Decode(&precio)
+		precios <- precio
+		res.Body.Close()
+	}
+}
 
-// func preciosCollector(precios <-chan precioTienda, errores <-chan error, done chan<- bool) {
+func preciosCollector(precios <-chan precioTienda, errores <-chan error, numFaltantes int, done chan<- error) {
+	numPrecios := 0
+	for {
+		select {
+		case p := <-precios:
+			if _, ok := cache[p.ID]; !ok {
+				cache[p.ID] = make(map[string]int)
+			}
+			cache[p.ID][p.Tienda] = p.Precio
+			numPrecios++
+			if numPrecios == numFaltantes {
+				done <- nil
+				return
+			}
+		case err := <-errores:
+			done <- err
+			return
+		}
+	}
+}
 
-// }
-
-// CalcularPrecios calcula los precios para la lista de IDs de productos pIDs en
-// los supermercados indicados en sIDs y devuelve una lista de Carrito
-func CalcularPrecios(pIDs []int, sIDs []string) ([]Carrito, error) {
+func calcularCarritos(pIDs []int, sIDs []string) []Carrito {
 	precios := make(map[string]int) // Mapeo de supermercado a precio total
-	fmt.Println("CalcularPrecios", pIDs, sIDs)
 	for _, pID := range pIDs {
 		for _, sID := range sIDs {
-			// fmt.Printf(apiURL, sID, pID)
+			precios[sID] += cache[pID][sID]
+		}
+	}
+	var res []Carrito
+	for k, v := range precios {
+		res = append(res, Carrito{k, v})
+	}
+	return res
+}
+
+func listaPreciosFaltantes(pIDs []int, sIDs []string) []precioTienda {
+	preciosFaltantes := []precioTienda{}
+	for _, pID := range pIDs {
+		for _, sID := range sIDs {
 			_, prodInCache := cache[pID]
 			priceInCache := false
 			if prodInCache {
 				_, priceInCache = cache[pID][sID]
 			}
 			if !priceInCache {
-				fmt.Printf("Getting price of %d from %s\n", pID, sID)
-
-				req, err := http.NewRequest(http.MethodGet, fmt.Sprintf(apiURL, sID, pID), nil)
-				if err != nil {
-					return nil, fmt.Errorf("No se pudo obtener el precio para el producto %d en el supermercado %s", pID, sID)
-				}
-				res, err := http.DefaultClient.Do(req)
-				if err != nil || res.StatusCode != http.StatusOK {
-					return nil, fmt.Errorf("No se pudo obtener el precio para el producto %d en el supermercado %s", pID, sID)
-				}
-				var precio precioTienda
-				json.NewDecoder(res.Body).Decode(&precio)
-				if _, ok := cache[precio.ID]; !ok {
-					cache[precio.ID] = make(map[string]int)
-				}
-				cache[precio.ID][precio.Tienda] = precio.Precio
-				res.Body.Close()
+				preciosFaltantes = append(preciosFaltantes, precioTienda{sID, pID, 0})
 			}
-			precios[sID] += cache[pID][sID]
 		}
 	}
-	// preciosAObtener := make(chan precioTienda)
-	// precios := make(chan precioTienda)
-	// suma := make(chan int)
-	// w := 0
-	// for w < numWorkers {
-	// 	go preciosWorker(preciosAObtener, precios, errores)
-	// 	w++
-	// }
-	// Transformo el map en la respuesta
-	var res []Carrito
-	for k, v := range precios {
-		res = append(res, Carrito{k, v})
+	return preciosFaltantes
+}
+
+func obtenerPreciosFaltantesDeAPI(pIDs []int, sIDs []string) error {
+	preciosFaltantes := listaPreciosFaltantes(pIDs, sIDs)
+
+	if len(preciosFaltantes) > 0 {
+		// Inicializacion de channels
+		preciosAObtener := make(chan precioTienda, len(preciosFaltantes))
+		precios := make(chan precioTienda)
+		errores := make(chan error)
+		done := make(chan error)
+
+		// Precarga de precios a obtener
+		for i := range preciosFaltantes {
+			preciosAObtener <- preciosFaltantes[i]
+		}
+		close(preciosAObtener)
+
+		// Inicializacion de workers
+		w := 0
+		for w < numWorkers {
+			go preciosWorker(preciosAObtener, precios, errores)
+			w++
+		}
+
+		go preciosCollector(precios, errores, len(preciosFaltantes), done)
+		err := <-done
+		if err != nil {
+			return err
+		}
 	}
-	return res, nil
+	return nil
+}
+
+// CalcularPrecios calcula los precios para la lista de IDs de productos pIDs en
+// los supermercados indicados en sIDs y devuelve una lista de Carrito
+func CalcularPrecios(pIDs []int, sIDs []string) ([]Carrito, error) {
+	err := obtenerPreciosFaltantesDeAPI(pIDs, sIDs)
+	if err != nil {
+		return nil, err
+	}
+	return calcularCarritos(pIDs, sIDs), nil
 }
