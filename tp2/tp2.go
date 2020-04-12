83c9712a7e4e3e0f2f5d483f290cbc743cca48bb
JesusELuna <jesusemanuelluna@gmail.com>
2020-04-12 17:27:59 -0300

    WIP Tp 2

--- tp2/tp2.go
+++ tp2/tp2.go
@@ -1,9 +1,34 @@
 package tp2
 
+import (
+	"fmt"
+)
+
 // SumarLista recibe una function sumadora y una lista
 // de numeros. Usando esos parametro retorna la suma de todos
 // los numeros. Si la suma no se puede realizar por algun motivo
 // se retorna un error.
 func SumarLista(sumFunc sumador, numeros ...int) (int, error) {
-	return sumFunc(numeros[0], numeros[1]), nil
+	nums := make(chan int, len(numeros))
+	result := make(chan int)
+	if len(numeros) < 2 {
+		return 0, fmt.Errorf("No se puede")
+	}
+	for _, num := range numeros {
+		nums <- num
+	}
+	close(nums)
+
+	go func() {
+		var total int
+		for n := range nums {
+			total = sumFunc(total, n)
+		}
+		result <- total
+	}()
+
+	total := <-result
+	close(result)
+
+	return total, nil
 }
