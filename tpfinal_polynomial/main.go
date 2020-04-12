923437935b69119d3162ed4e9a3c749577a9e188
Brunoli <hortelanobruno@gmail.com>
2020-04-12 14:38:32 -0300

    iteracion tp finales

--- tpfinal_polynomial/main.go
+++ tpfinal_polynomial/main.go
@@ -5,18 +5,126 @@ import (
 	"log"
 	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 
-	"math/rand"
-
 	"github.com/guptarohit/asciigraph"
-	"gonum.org/v1/plot/plotter"
 )
 
 type polynomial struct {
 	coef   []int
 	degree int
+}
+
+func (p *polynomial) roots() []float64 {
+	var roots []float64
+	var coefs []float64
+	for i := 0; i < len(p.coef); i++ {
+		coefs = append(coefs, float64(p.coef[i]))
+	}
+	return p.roots2(coefs, roots)
+}
+
+func (p *polynomial) roots2(coefs []float64, roots []float64) []float64 {
+	if len(p.coef) > 3 {
+		k := p.findK(coefs)
+		if k != nil {
+			roots = append(roots, *k)
+			coefs = p.dividePolynomialByXMinusK(coefs, k)
+			return p.roots2(coefs, roots)
+		}
+	} else if len(p.coef) == 3 {
+		roots = append(roots, p.getQuadraticRoots(coefs[0], coefs[1], coefs[2])...)
+	} else if len(coefs) == 2 {
+		roots = append(roots, p.getQuadraticRoots(0, coefs[0], coefs[1])...)
+	}
+	sort.Float64s(roots)
+	for i := 0; i < len(roots); i++ {
+		if roots[i] == 0 {
+			roots[i] = 0
+		}
+		for j := i + 1; j < len(roots); j++ {
+			if roots[i] == roots[j] {
+				roots = append(roots[:j], roots[j+1:]...)
+				j--
+			}
+		}
+	}
+	return roots
+}
+
+func (p *polynomial) findK(coefs []float64) *float64 {
+	if len(coefs) > 2 {
+		constant := coefs[len(coefs)-1]
+		if constant == 0 {
+			a := float64(0)
+			return &a
+		}
+		var trialKValues []float64
+
+		for i := 1; i <= int(math.Abs(constant)); i++ {
+			if int(math.Abs(constant))%i == 0 {
+				trialKValues = append(trialKValues, float64(i))
+				trialKValues = append(trialKValues, float64(i*-1))
+			}
+		}
+
+		for _, k := range trialKValues {
+			sumOfTerms := float64(0)
+			for i := 0; i < len(coefs); i++ {
+				sumOfTerms += coefs[i] * math.Pow(k, float64(len(coefs)-1-i))
+			}
+			if sumOfTerms == 0 {
+				return &k
+			}
+		}
+	}
+	return nil
+}
+
+func (p *polynomial) dividePolynomialByXMinusK(coefs []float64, k *float64) []float64 {
+	if k != nil && coefs != nil && len(coefs) > 2 {
+		var newCoefficients []float64
+		newCoefficients = append(newCoefficients, coefs[0])
+
+		for i := 1; i < len(coefs)-1; i++ {
+			newCoefficients = append(newCoefficients, newCoefficients[i-1]**k+coefs[i])
+		}
+
+		lastIndex := len(coefs) - 1
+
+		if newCoefficients[lastIndex-1]**k+coefs[lastIndex] != 0 {
+			return nil
+		}
+
+		return newCoefficients
+	}
+	return nil
+}
+
+func (p *polynomial) getQuadraticRoots(a float64, b float64, c float64) []float64 {
+	var roots []float64
+
+	if a != 0 {
+		nRoots := 0
+		discriminant := math.Pow(b, 2) - 4*a*c
+		if discriminant > 0 {
+			nRoots = 2
+		} else if discriminant == 0 {
+			nRoots = 1
+		}
+
+		if nRoots == 1 {
+			roots = append(roots, b*-1/(2*a))
+		} else if nRoots == 2 {
+			roots = append(roots, ((b*-1 + math.Sqrt(discriminant)) / (2 * a)))
+			roots = append(roots, ((b*-1 - math.Sqrt(discriminant)) / (2 * a)))
+		}
+	} else {
+		roots = append(roots, (c*-1)/b)
+	}
+	return roots
 }
 
 func (p *polynomial) evaluate(x float64) float64 {
@@ -106,9 +214,21 @@ func main() {
 	log.Printf("Degree: %d", p.degree)
 	log.Printf("Coef: %v", p.coef)
 
-	var ys []float64
+	r := p.roots()
+	log.Printf("Real Roots: %v", r)
 
-	for x := -5; x < 5; x++ {
+	var ys []float64
+	var start, end float64
+
+	if len(r) == 0 {
+		start = -5
+		end = 5
+	} else {
+		start = r[0] - 1
+		end = r[len(r)-1] + 1
+	}
+
+	for x := start; x <= end; x++ {
 		y := p.evaluate(float64(x))
 		ys = append(ys, y)
 	}
@@ -117,18 +237,5 @@ func main() {
 	graph := asciigraph.Plot(data)
 
 	fmt.Println(graph)
-}
 
-// randomPoints returns some random x, y points.
-func randomPoints(n int) plotter.XYs {
-	pts := make(plotter.XYs, n)
-	for i := range pts {
-		if i == 0 {
-			pts[i].X = rand.Float64()
-		} else {
-			pts[i].X = pts[i-1].X + rand.Float64()
-		}
-		pts[i].Y = pts[i].X + 10*rand.Float64()
-	}
-	return pts
 }
