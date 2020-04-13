de5fe12876a832d2466ae32e59d0c76c1012ea8f
JesusELuna <jesusemanuelluna@gmail.com>
2020-04-13 17:14:07 -0300

    TP 3

--- tp3/tp3.go
+++ tp3/tp3.go
@@ -33,5 +33,23 @@ type Resultado struct {
 }
 
 func Calcular(sumas, mults, divisiones, restas <-chan *Operandos, corte <-chan struct{}) chan *Resultado {
-	return nil
+	res := make(chan *Resultado, 4)
+
+	for {
+		select {
+		case op := <-sumas:
+			res <- &Resultado{Operacion: SUMA, Resultado: float64(op.A + op.B)}
+		case op := <-mults:
+			res <- &Resultado{Operacion: MULT, Resultado: float64(op.A * op.B)}
+		case op := <-divisiones:
+			res <- &Resultado{Operacion: DIVISION, Resultado: float64(op.A / op.B)}
+		case op := <-restas:
+			res <- &Resultado{Operacion: RESTA, Resultado: float64(op.A - op.B)}
+		default:
+			if len(res) == cap(res) {
+				close(res)
+				return res
+			}
+		}
+	}
 }
