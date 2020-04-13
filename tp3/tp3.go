5f706df1381f08f0577edb8c93c0596e94644c27
costaelias <ecosta@est.frba.utn.edu.ar>
2020-04-13 15:45:29 -0300

    Resolucion TP1, TP2, TP3

--- tp3/tp3.go
+++ tp3/tp3.go
@@ -1,5 +1,7 @@
 package tp3
 
+import "fmt"
+
 type Op int
 
 const (
@@ -32,6 +34,54 @@ type Resultado struct {
 	Resultado float64
 }
 
+func CalculoAuxiliar(sumas, mults, divisiones, restas <-chan *Operandos, corte <- chan struct{}, resultados chan <- *Resultado){
+
+
+	for{
+		select {
+		case s:=<- sumas:
+			
+			resSuma := float64(s.A + s.B)
+
+			resultados <- &Resultado{SUMA,resSuma}
+
+		case r:=<- restas:
+
+			resResta := float64(r.A - r.B)
+
+			resultados <- &Resultado{RESTA,resResta}
+
+		case m:=<- mults:
+			
+			resMulti:= float64(m.A * m.B)
+
+			resultados <- &Resultado{MULT, resMulti}
+
+		case d:=<-divisiones:
+			
+			if d.B != 0{
+
+				resDiv := float64(d.A / d.B)
+
+				resultados <- &Resultado{DIVISION,resDiv}
+			}
+
+		case <-corte:
+			fmt.Println("Fin de las operaciones")
+			close(resultados)
+			
+		}
+	}
+	close(resultados)
+	
+}
+
 func Calcular(sumas, mults, divisiones, restas <-chan *Operandos, corte <-chan struct{}) chan *Resultado {
-	return nil
+
+	resutlados := make(chan *Resultado, 4)
+
+	go CalculoAuxiliar(sumas, mults, divisiones, restas, corte, resutlados)
+
+	return resutlados
+
 }
