5bb6354ce69b7a2cab5f53c57a03a31d59a1b67e
SantiagoP <piolisantiago@gmail.com>
2020-06-11 17:15:52 -0300

    Resolución del TP1
    Mejor tarde que nunca...
    Vengo muy oxidado de programar asique espero sepan disculpas las desprolijidades. Me corrieron OK los tests. Sigo con los siguientes TPs. Excelente trabajo locos, signa así.

--- tp1/tp1.go
+++ tp1/tp1.go
@@ -1,5 +1,10 @@
 package tp1
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // Producto contiene metodos que nos permiten acceder
 // a atributos que esperamos de un Producto.
 type Producto interface {
@@ -13,6 +18,25 @@ type Producto interface {
 // que carga informacion guardada en `productos.json`.
 type Productos [][]string
 
+const (
+	SUPERMERCADO_INDEX = 0
+	PRODUCTO_INDEX     = 1
+	PRECIO_INDEX       = 2
+)
+
+type UnProducto struct {
+	id     int
+	precio int
+}
+
+func (p UnProducto) ID() int {
+	return p.id
+}
+
+func (p UnProducto) Precio() int {
+	return p.precio
+}
+
 // Carrito contiene el nombre de la tienda y el precio final luego
 // de sumar todos los productos.
 type Carrito struct {
@@ -24,17 +48,85 @@ type Carrito struct {
 // para cada super mercado, cuanto saldria comprar esos productos ahi.
 // Retorna un slice de carritos, donde se tiene uno para cada super mercado.
 func (p Productos) CalcularPrecios(ids ...int) []Carrito {
-	return nil
+	//return nil
+
+	var carrito []Carrito
+	mapaProductos := make(map[string]int)
+
+	for _, prodInput := range ids {
+		for _, prodLista := range p {
+			productoActual, err := strconv.Atoi(prodLista[PRODUCTO_INDEX])
+			if err != nil {
+				fmt.Println("Error handling string conversion", productoActual)
+				return nil
+			}
+			if prodInput == productoActual {
+				productoActualPrecio, err := strconv.Atoi(prodLista[PRECIO_INDEX])
+				if err != nil {
+					fmt.Println("Error handling string conversion", productoActual)
+					return nil
+				}
+				mapaProductos[prodLista[SUPERMERCADO_INDEX]] += productoActualPrecio
+			}
+
+		}
+	}
+	for supermercado, precio := range mapaProductos {
+		carrito = append(carrito, Carrito{Tienda: supermercado, Precio: precio})
+	}
+
+	return carrito
 }
+
 
 // Promedio recibe el id de un producto y retorna el precio promedio
 // de ese producto usando los precios de todos los supermercados.
 func (p Productos) Promedio(idProducto int) float64 {
-	return 0
+	var precioTotal float64
+	var superCant int
+
+	for _, prodLista := range p {
+		productoActual, err := strconv.Atoi(prodLista[PRODUCTO_INDEX])
+		if err != nil {
+			fmt.Println("Error handling string conversion")
+			return 0
+		}
+		if idProducto == productoActual {
+			productoActualPrecio, err := strconv.Atoi(prodLista[PRECIO_INDEX])
+			if err != nil {
+				fmt.Println("Error handling product price in Productos")
+				return 0
+			}
+			precioTotal += float64(productoActualPrecio)
+			superCant++
+		}
+	}
+	if superCant > 0 {
+		return float64(precioTotal) / float64(superCant)
+	}
+	return 0.0
 }
 
 // BuscarMasBarato recibe un id de producto a buscar y te retorna
 // el producto mas barato que haya encontrado.
 func (p Productos) BuscarMasBarato(idProducto int) (Producto, bool) {
-	return nil, false
+	//return nil, false
+	menorPrecio := 0
+	existe := false
+	for _, prodLista := range p {
+		productoActual, err := strconv.Atoi(prodLista[PRODUCTO_INDEX])
+		precioActual, err := strconv.Atoi(prodLista[PRECIO_INDEX])
+		if err != nil {
+			fmt.Println("Error handling producto price in Productos")
+			return nil, false
+		}
+		if idProducto == productoActual {
+			if existe == false || precioActual < menorPrecio {
+				menorPrecio = precioActual
+				existe = true
+			}
+		}
+	}
+	return UnProducto{id: idProducto, precio: menorPrecio}, existe
+
 }
